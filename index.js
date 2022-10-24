d5f45ce6b231623240387ce783dd0daf36a6eee9
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-10-24 14:50:03 -0300

    feat: first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Rekognition } = require("aws-sdk");
+const fs = require("fs");
+
+const rekognition = new Rekognition({
+    region: "sa-east-1"
+})
+
+rekognition.detectFaces({
+    Attributes: ["ALL"],
+    Image: {
+        Bytes: fs.readFileSync("./Imagem do iOS.jpg")
+    }
+}, (err, data) => {
+    if(err) {
+        console.log(err, err.stack)
+    } else {
+        console.log(data)
+    }
+}
+)
