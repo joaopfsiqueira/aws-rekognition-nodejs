3d84009634561d241569e7935da000ec83cb7d22
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-10-26 20:01:23 -0300

    feat: criando coleções de imagens

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ const rekognition = new Rekognition({
     region: "us-east-1"
 })
 
-rekognition.detectFaces({
-    Attributes: ["ALL"],
-    Image: {
-        Bytes: fs.readFileSync(path.resolve(__dirname, "./Imagem do iOS.jpg"))
+
+rekognition.createCollection(
+    {
+        CollectionId: "default" //nome para a collection
+    },
+    function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data)
     }
-}, (err, data) => {
-    if(err) {
-        console.log(err, err.stack)
-    } else {
-        // console.log(JSON.stringify(data.FaceDetails))
-        fs.writeFileSync(path.resolve(__dirname, "./result.json"), JSON.stringify(data))
-    }
-}
 )
+
+
+// rekognition.detectFaces({
+//     Attributes: ["ALL"],
+//     Image: {
+//         Bytes: fs.readFileSync(path.resolve(__dirname, "./Imagem do iOS.jpg"))
+//     }
+// }, (err, data) => {
+//     if(err) {
+//         console.log(err, err.stack)
+//     } else {
+//         // console.log(JSON.stringify(data.FaceDetails))
+//         fs.writeFileSync(path.resolve(__dirname, "./result.json"), JSON.stringify(data))
+//     }
+// }
+// )
